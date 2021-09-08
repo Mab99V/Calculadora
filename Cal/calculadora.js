1bc67491c04b9db8c34d39c730d9bc8488160f66
Mab99V <mabelperezgaribay@gmail.com>
2021-09-08 18:04:07 -0500

    Prueba1 Calculadora

---
+++ Cal/calculadora.js
@@ -0,0 +1,107 @@
+var  a
+var  b
+var  resultado
+
+function  sumar( )  {
+    var  numero1 = documento.getElementById ("a");
+    var  numero2 = documento.getElementById ("b");
+    
+    a = numero1.valor;
+    b = numero2.valor;
+    
+    consola.registro(a,b);
+    resultado = parseInt(a) + parseInt(b);
+    consola.log(resultado);
+
+    var  areaElement = documento . getElementById ("resultado");
+    areaElement.innerHTML = 'Resultado =' + resultado;
+}
+
+function  restar( )  {
+    var  numero1 = documento.getElementById("a");
+    var  numero2 = documento.getElementById("b");
+    
+    a = numero1.valor ;
+    b = numero2.valor;
+    
+    consola.registro(a,b);
+    resultado = parseInt ( a ) - parseInt ( b );
+    consola.log(resultado);
+
+    var  areaElement = documento.getElementById ("resultado") ;
+    areaElement.innerHTML = 'Resultado =' + resultado;
+
+
+}
+
+function  multiplicar( )  {
+    var  numero1 = documento.getElementById ("a");
+    var  numero2 = documento.getElementById ("b");
+    
+    a = numero1.valor;
+    b = numero2.valor;
+    
+    consola.registro(a , b);
+    resultado = a * b;
+    consola.log (resultado);
+
+    var  areaElement = documento.getElementById ("resultado") ;
+    areaElement.innerHTML = 'Resultado =' + resultado;
+
+
+}
+
+function  dividir( )  {
+    var  numero1 = documento.getElementById ("a");
+    var  numero2 = documento.getElementById ("b");
+    
+    a = numero1.valor ;
+    b = numero2.valor;
+    
+    consola.registro(a,b);
+    resultado= a/b;
+    consola.log(resultado);
+
+    var  areaElement = documento.getElementById ("resultado") ;
+    areaElement.innerHTML ='Resultado ='+ resultado;
+}
+function  elevar( )  {
+    var  numero1 = documento.getElementById("a");
+
+    a = numero1.valor;
+    
+    consola.registro(a);
+    resultado = Math.pow(a,2);
+    consola.log(resultado);
+
+    var  areaElement = documento.getElementById("resultado");
+    areaElement.innerHTML = 'Resultado =' + resultado;
+}
+
+function  raiz( )  {
+    var  numero1 = documento.getElementById("a");
+
+    a = numero1.valor;
+    
+    consola.registro(a);
+    resultado = Math.sqrt(a);
+    consola.log(resultado);
+
+    var  areaElement = documento.getElementById("resultado");
+    areaElement.innerHTML = 'Resultado =' + resultado;
+}
+
+function  elevarNumero( )  {
+    var  numero1  = documento.getElementById("a");
+    var  numero2  = documento.getElementById ("b");
+
+    a = numero1.valor;
+    b = numero2.valor;
+    
+    consola.registro(a,b);
+    resultado = Math.pow(a,b);
+    consola.log(resultado );
+
+    var  areaElement = documento.getElementById("resultado");
+    areaElement.innerHTML = 'Resultado =' + resultado;
+}
